1e27c9fc543ed647e8c139e61d81afd8ec29a56b
Almightyhao <a0979615243@gmail.com>
2025-10-30 01:57:03 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+            apiKey: "AIzaSyAbzAhf0_G099NiJQ_bGJAAxK6_HkiQToA",
+            authDomain: "soap-writter.firebaseapp.com",
+            projectId: "soap-writter",
+            storageBucket: "soap-writter.firebasestorage.app",
+            messagingSenderId: "806633094247",
+            appId: "1:806633094247:web:d61e064b3dd0682f30e8c2"
+        };
